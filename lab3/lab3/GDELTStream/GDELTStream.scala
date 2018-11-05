8d91b87c53269d408b7e8442121c5c8ee1bc9922
Nicolas Chaves de Plaza <chadepl@gmail.com>
2018-11-05 22:33:36 +0100

    Logic changed to take into account last hour records

--- lab3/lab3/GDELTStream/GDELTStream.scala
+++ lab3/lab3/GDELTStream/GDELTStream.scala
@@ -41,12 +41,20 @@ object GDELTStream extends App {
   // only without dates! After this apply the HistogramTransformer. Finally, 
   // write the result to a new topic called gdelt-histogram. 
 
-    val countStoreSupplier: StoreBuilder[KeyValueStore[String, Long]] =
+  val countStoreSupplier: StoreBuilder[KeyValueStore[String, Long]] =
     Stores.keyValueStoreBuilder(
     Stores.persistentKeyValueStore("Counts"),
     Serdes.String,
     Serdes.Long)
-    builder.addStateStore(countStoreSupplier);
+
+  val fullLogStoreSupplier: StoreBuilder[KeyValueStore[String, String]] =
+    Stores.keyValueStoreBuilder(
+    Stores.persistentKeyValueStore("fullLog"),
+    Serdes.String,
+    Serdes.String)
+
+  builder.addStateStore(countStoreSupplier);
+  builder.addStateStore(fullLogStoreSupplier);
 
   // check time format in doc for key 
   val records: KStream[String, String] = builder.stream[String, String]("gdelt")
@@ -56,10 +64,13 @@ object GDELTStream extends App {
   val formatted = data.map((key, words) => (key, words.map(x => x.split(",")(0))))
   val flat = formatted.flatMapValues(x => x)
 
-  val outputStream = flat.transform(new HistogramTransformer(), "Counts")
+  val outputStream = flat.transform(new HistogramTransformer(), "Counts", "fullLog")
+  
   outputStream.foreach((key,value) => {
-    println(key)
-    println(value)
+    //if(value < 0) {
+        println(key)
+        println(value)
+      //}
   })
   outputStream.to("gdelt-histogram")
 
@@ -85,29 +96,72 @@ object GDELTStream extends App {
 class HistogramTransformer extends Transformer[String, String, (String, Long)] {
   var context: ProcessorContext = _
   var state: KeyValueStore[String, Long] = _
+  var fullLog: KeyValueStore[String, String] = _
+
+  val df: SimpleDateFormat = new SimpleDateFormat("yyyyMMddHHmm")
+  val TimeWindowWidth = 60 // 3600 secs for one hour
+  val HistogramRefreshRate = 1000 
 
   // Initialize Transformer object
   def init(context: ProcessorContext){
     this.context = context
     this.state = context.getStateStore("Counts").asInstanceOf[KeyValueStore[String, Long]]
+    this.fullLog = context.getStateStore("fullLog").asInstanceOf[KeyValueStore[String, String]]
+      
+    // This updates the window of the histogram each second
+    this.context.schedule(this.HistogramRefreshRate, PunctuationType.WALL_CLOCK_TIME, (timestamp) => {
+      computeHistogram()  
+    })
   }
 
-  // Should return the current count of the name during the _last_ hour
+  def computeHistogram() = {
+    println("Recomputing histogram ...")
+    val recordsIterator = fullLog.all()//.asInstanceOf[KeyValueIterator[String, Long]]
+    var count = 0
+    val currentDate: Date  = df.parse(df.format(Calendar.getInstance().getTime()))
+    while(recordsIterator.hasNext){
+      val record = recordsIterator.next()
+      try{ // dealing with records comming with different formats
+        val recordTimestamp = record.key.split("---")(0).toLong
+        val recordDate: Date  = new Date(recordTimestamp)
+        if((currentDate.getTime()  - recordDate.getTime()) / 1000 > this.TimeWindowWidth){ 
+          println(record.value + " OUT!") 
+          println("--- name: " + record.value)
+          println("--- value before: " + state.get(record.value))
+          state.put(record.value, state.get(record.value)-1) 
+          println("--- value after: " + state.get(record.value))
+          fullLog.delete(record.key)        
+          context.forward(record.value, state.get(record.value))
+        }
+      } catch {          
+          case e: Exception => {
+            //println(record)
+            //println(e)            
+          }
+      }
+    }     
+  }
+
+
+  //def clean
+
+
   def transform(key: String, name: String): (String, Long) = {
-    val df: SimpleDateFormat = new SimpleDateFormat("yyyyMMddHHmm")
-    val count: Optional[Long] = Optional.ofNullable(state.get(name))
-    val date1: Date  = new Date(context.timestamp)
-    val date2: Date  = df.parse(df.format(Calendar.getInstance().getTime()))
-    var incrementedCount: Long = 0L
-    // if record is within the hour add one to the counter
-    if((date2.getTime() - date1.getTime()) / 1000 < 3600){
-      incrementedCount = count.orElse(0L) + 1
-      state.put(name, incrementedCount)
-      // schedule to remove the count after 1 hour
-      this.context.schedule(60000, PunctuationType.WALL_CLOCK_TIME, (timestamp) => {
-        state.put(name, state.get(name)-1)
-      })
-    }
+    val recordDate: Date  = new Date(context.timestamp)
+    val currentDate: Date  = df.parse(df.format(Calendar.getInstance().getTime()))
+    
+    val currentCount: Optional[Long] = Optional.ofNullable(state.get(name)) //TODO: Still getting negative numbers sometimes
+    var incrementedCount: Long = currentCount.orElse(0L)
+
+    //println(recordDate.getTime() + "$" + name)
+    
+    fullLog.put(recordDate.getTime() + "---" + name, name) // This is the windowed log
+    
+    if ((currentDate.getTime() - recordDate.getTime()) / 1000 < this.TimeWindowWidth) 
+      incrementedCount = incrementedCount + 1 
+    
+    state.put(name, incrementedCount)      
+
     return (name, incrementedCount)
   }
   // Close any resources if any
